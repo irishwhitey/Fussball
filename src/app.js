f29eaa8f4d8fa5e9dc938f891fca30f50d4a4861
unknown <martin@shinylaptop.Flat3Domain.Local>
2015-05-28 00:38:17 +0100

    Capialised the players Names

--- src/app.js
+++ src/app.js
@@ -9,16 +9,6 @@ var source1 = fs.readFileSync(__dirname +"/views/index.html", {
 	encoding : "utf8"
 });
 
-Handlebars.registerHelper('listRawResults', function(results) {
-  var out = "<ul>";
-	console.log(results)
-  for(var i=0, l=results.length; i<l; i++) {
-    out = out + "<li>" + results["home team"] + "</li>";
-  }
-
-  return out + "</ul>";
-});
-
 var indexTemplate = Handlebars.compile(source1)
 
 app.get('/', function (req, res) {
